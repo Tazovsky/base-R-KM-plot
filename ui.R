b41898c50ed23e450fa199563afe2bea05f4efaa
trestletech <jeff.allen@trestletechnology.net>
2013-07-04 08:24:22 -0500

    Arrange genes.

--- ui.R
+++ ui.R
@@ -1,6 +1,6 @@
 library(shiny)
 
-geneList <- c("EGFR", "KLF6", "FOXO1", "KRAS", "JAK2", "BRCA1", "BRCA2", "PPM1D")
+geneList <- c( "KRAS", "EGFR", "KLF6", "FOXO1", "JAK2", "BRCA1", "BRCA2", "PPM1D")
 
 shinyUI(pageWithSidebar(
   # Application title
